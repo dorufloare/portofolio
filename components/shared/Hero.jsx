2f0d66cab3e906d947b58aa5d22017ff7b3b176b
dorufloare <dorufloare@yahoo.com>
2025-07-21 21:23:17 +0200

    mobile

--- components/shared/Hero.jsx
+++ components/shared/Hero.jsx
@@ -2,10 +2,10 @@
 
 const Hero = () => {
   return (
-    <section className="w-full min-h-screen flex flex-col xl:flex-row items-center justify-center px-6 md:px-24 xl:px-32 2xl:px-48 py-16 gap-10 xl:gap-20 max-w-[1600px] mx-auto">
+    <section className="mb-48 md:mb-48 mt-52 md:mt-0 w-full h-[600px] md:h-screen flex flex-col xl:flex-row items-center justify-center px-6 md:px-24 xl:px-32 2xl:px-48 py-8 md:py-16 gap-1 xl:gap-20 max-w-[1600px] mx-auto">
       {/* Left: Intro Text */}
       <div className="flex-1 flex flex-col items-start text-center xl:text-left">
-        <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold text-white mb-4 leading-tight">
+        <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold text-white mb-2 md:mb-4 mt-12 md:mt-0 leading-tight">
           Hi, I’m <br />
           <span className="text-[#3399ff]">Doru Floare</span>
         </h1>
@@ -16,10 +16,9 @@ const Hero = () => {
           and Software Engineer
         </p>
       </div>
-
       {/* Right: Terminal Code Block */}
-      <div className="flex-1 w-full max-w-3xl bg-[#0e1117] text-white rounded-xl p-6 md:p-8 shadow-lg border border-[#1f2733]">
-        <pre className="whitespace-pre-wrap break-words text-sm md:text-base font-mono leading-relaxed">
+      <div className="flex-1 w-full max-w-3xl bg-[#0e1117] text-white rounded-xl p-3 md:p-8 shadow-lg border border-[#1f2733] max-h-40 md:max-h-none overflow-hidden md:mt-0">
+        <pre className="whitespace-pre-wrap break-words text-[10px] md:text-base font-mono leading-tight md:leading-relaxed">
           <code>
             <span className="text-[#c678dd]">#include</span>{" "}
             <span className="text-[#98c379]">&lt;iostream&gt;</span>
