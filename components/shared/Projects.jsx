a3d70574cf584687ff7bd64cd0d3e164376692fc
dorufloare <dorufloare@yahoo.com>
2025-08-16 20:57:36 +0200

    bugfix

--- components/shared/Projects.jsx
+++ components/shared/Projects.jsx
@@ -69,7 +69,7 @@ const Projects = () => {
       <h1 className="text-center text-2xl md:text-4xl font-bold mb-6 md:mb-24">
         Some of My Projects
       </h1>
-      <div className="grid grid-cols-1 px-[12%] sm:px-0 sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4 sm:gap-6 xl:gap-6 justify-items-center">
+      <div className="grid grid-cols-1 px-[8%] sm:px-0 sm:grid-cols-2 lg:grid-cols-3 3xl:grid-cols-4 gap-4 sm:gap-6 xl:gap-6 justify-items-center">
         {products.map((product, index) => (
           <Card
             key={index}
